e52ca730c8bbf46ca02ee19af8a386efd3a8e6a3
Pedro Lucas <pedrolucastrainee@gmail.com>
2021-09-23 18:52:06 -0300

    feat: data project

--- src/compenents/works/Works.jsx
+++ src/compenents/works/Works.jsx
@@ -1,43 +1,14 @@
 import { useState } from "react"
 import "./works.scss"
 
+import { data } from "../../data";
 export default function Works() {
     const [currentSlide, setcurrentSlide] = useState(0);
-    const data = [
-        {
-            id: "1",
-            icon: "./assets/mobile.png",
-            title: "Trabalho 1",
-            desc:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
-            img:
-                "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
-        },
-        {
-            id: "2",
-            icon: "./assets/globe.png",
-            title: "Trabalho 2",
-            desc:
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-            img:
-                "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
-        },
-        {
-            id: "3",
-            icon: "./assets/writing.png",
-            title: "Trabalho 3",
-            desc:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-            img:
-            "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
-        },
-    ]
 
     const handleClink = (way) => {
         way === "left" ?  setcurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2) :
         setcurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0)
     }
-
     return (
         <div className="works" id="works">
             <h1>Projetos</h1>
