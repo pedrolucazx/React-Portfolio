e6ca05ad56a613813fafe64caa92c199ceaec18b
Pedro Lucas <pedrolucastrainee@gmail.com>
2021-09-24 02:26:30 -0300

    Finish

--- src/compenents/contact/Contact.jsx
+++ src/compenents/contact/Contact.jsx
@@ -1,30 +1,31 @@
-import {LinkedIn, Mail, GitHub} from "@material-ui/icons";
+import { LinkedIn, Mail, GitHub } from "@material-ui/icons";
 import "./contact.scss"
 
 export default function Contact() {
-    return (
-        <div className="contact" id="contact">
-            <div className="left">
-                <img src="assets/shake.svg" alt="" />
-            </div>
+  return (
+    <div className="contact" id="contact">
+      
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
 
-            <div className="right">
-            <h2>Contato</h2>
-              <div className="sociais">
-                <div className="itemContainer">
-                  <LinkedIn className="icon"/>
-                  <a href="https://www.linkedin.com/in/pedrolucazx/" className="links" target="blank">LinkedIn</a>
-                </div>
-                <div className="itemContainer">
-                  <Mail className="icon"/>
-                  <a href="mailto:pedrolucas0921@gmail.com" className="links" target="blank">Mail</a>
-                </div>
-                <div className="itemContainer">
-                  <GitHub className="icon"/>
-                    <a href="https://github.com/pedrolucazx" className="links" target="blank">GitHub</a>
-                </div>
-              </div>
-            </div>
+      <div className="right">
+        <h2>Contato</h2>
+        <div className="sociais">
+          <div className="itemContainer">
+            <LinkedIn className="icon" />
+            <a href="https://www.linkedin.com/in/pedrolucazx/" className="links" target="blank">LinkedIn</a>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <a href="mailto:pedrolucas0921@gmail.com" className="links" target="blank">Mail</a>
+          </div>
+          <div className="itemContainer">
+            <GitHub className="icon" />
+            <a href="https://github.com/pedrolucazx" className="links" target="blank">GitHub</a>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
--- src/compenents/works/Works.jsx
+++ src/compenents/works/Works.jsx
@@ -3,44 +3,44 @@ import "./works.scss"
 
 import { data } from "../../data";
 export default function Works() {
-    const [currentSlide, setcurrentSlide] = useState(0);
+  const [currentSlide, setcurrentSlide] = useState(0);
 
-    const handleClink = (way) => {
-        way === "left" ?  setcurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2) :
-        setcurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0)
-    }
-    return (
-        <div className="works" id="works">
-            <h1>Projetos</h1>
-            <div className="slider" style={{transform:`translateX(-${currentSlide * 100}vw)`}}>
-                {data.map( (d) => (
-                    <div className="container">
-                        <div className="item">
-                            <div className="left">
-                                <div className="leftContainer">
-                                    <div className="imgContainer">
-                                        <img src={d.icon} alt="" />
-                                    </div>
-                                    <h2>{d.title}</h2>
-                                    <p>
-                                        {d.desc}
-                                    </p>
-                                    <span>Projects</span>
-                                </div>
-                            </div>
-                            <div className="right">
-                            <img
-                                src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"
-                                alt=""
-                            />
-                            </div>
-                        </div>
-                    </div>
-                ))}
+  const handleClink = (way) => {
+    way === "left" ? setcurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2) :
+      setcurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0)
+  }
+  return (
+    <div className="works" id="works">
+      <h1>Projetos</h1>
+      <div className="slider" style={{ transform: `translateX(-${currentSlide * 100}vw)` }}>
+        {data.map((d) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src={d.icon} alt="" />
+                  </div>
+                  <h2>{d.title}</h2>
+                  <p>
+                    {d.desc}
+                  </p>
+                  <span>Projects</span>
+                </div>
+              </div>
+              <div className="right">
+                <img
+                  src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"
+                  alt=""
+                />
+              </div>
             </div>
-            <img src="assets/arrow.png" className="arrow left" alt="" onClick={() => handleClink("left")} />
-            <img src="assets/arrow.png" className="arrow right" alt="" onClick={() => handleClink()} />
+          </div>
+        ))}
+      </div>
+      <img src="assets/arrow.png" className="arrow left" alt="" onClick={() => handleClink("left")} />
+      <img src="assets/arrow.png" className="arrow right" alt="" onClick={() => handleClink()} />
 
-        </div>
-    )
+    </div>
+  )
 }
