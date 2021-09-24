d93cf50a708320788aa7864da572307cc80bbfb8
Pedro Lucas <pedrolucastrainee@gmail.com>
2021-09-24 02:15:56 -0300

    feat: responsive about component

--- src/compenents/about/About.jsx
+++ src/compenents/about/About.jsx
@@ -2,17 +2,15 @@ import './about.scss'
 export default function About() {
   return (
     <div className="about" id="about">
+
       <div className="skills">
-        <div className="title">
-          <h2>Habilidades & conhecimentos</h2>          
-        </div>
-        <div className="hardSkills">
+          <h2 className="hard">Habilidades & conhecimentos</h2>  
           <img src="https://img.icons8.com/color/144/000000/javascript--v1.png" alt="Logo JavaScript"/>
           <img src="https://img.icons8.com/color/144/000000/html-5--v1.png" alt="Logo HTML5" />
           <img src="https://img.icons8.com/color/144/000000/css3.png" alt="Logo CSS3"/>
           <img src="https://img.icons8.com/office/144/000000/react.png" alt="Logo ReactJS"/>
-        </div>
       </div>
+
       <div className="main-about">
         <h2>Sobre mim:</h2>
         <p>
@@ -22,6 +20,7 @@ export default function About() {
           porém sou completamente flexível em aprender uma nova linguagem ou tecnologia.
         </p>
       </div>
+
     </div>
   )
 }
